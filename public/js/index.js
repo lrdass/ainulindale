9fad0be5e648d98f6e9bcd83300047f70576c5f1
lrdass <lsuppes@pm.me>
2020-09-30 19:15:30 -0300

    renderizando strings

--- public/js/index.js
+++ public/js/index.js
@@ -5,19 +5,38 @@ const element = {
         id: "container",
         children: [
             { type: "a", props: { href: "/bar" } },
-            { type: "span", props: {} },
+            {
+                type: "span",
+                props: {
+                    children: [
+                        {
+                            type: "text_element",
+                            props: { nodeValue: "Foo" },
+                        },
+                    ],
+                },
+            },
         ],
     },
 };
 const render = (element, parentDom) => {
     const { type: tagName, props } = element;
-    const dom = document.createElement(tagName);
+    const isTextualElement = (tagName) => tagName === "text_element";
+    const dom = isTextualElement(tagName)
+        ? document.createTextNode(props === null || props === void 0 ? void 0 : props.nodeValue)
+        : document.createElement(tagName);
     const isListener = (name) => name.startsWith("on");
     Object.keys(props)
         .filter(isListener)
         .forEach((name) => {
         const eventType = name.toLowerCase().substring(2);
         dom.addEventListener(eventType, props[name]);
+    });
+    const isAttribute = (name) => !isListener && name !== "children";
+    Object.keys(props)
+        .filter(isAttribute)
+        .forEach((propName) => {
+        dom[propName] = props[propName];
     });
     const childElements = (props === null || props === void 0 ? void 0 : props.children) || [];
     childElements.forEach((child) => render(child, dom));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 interface AinuElement {
-  type?: string;
+  type: string;
   props?: AinuElementProps;
 }
 
@@ -15,14 +15,29 @@ const element: AinuElement = {
     id: "container",
     children: [
       { type: "a", props: { href: "/bar" } },
-      { type: "span", props: {} },
+      {
+        type: "span",
+        props: {
+          children: [
+            {
+              type: "text_element",
+              props: { nodeValue: "Foo" },
+            },
+          ],
+        },
+      },
     ],
   },
 };
 
 const render = (element: AinuElement, parentDom: HTMLElement) => {
   const { type: tagName, props } = element;
-  const dom = document.createElement(tagName as string);
+
+  const isTextualElement = (tagName: string) => tagName === "text_element";
+
+  const dom = isTextualElement(tagName)
+    ? document.createTextNode(props?.nodeValue)
+    : <any>document.createElement(<string>tagName);
 
   const isListener = (name: string) => name.startsWith("on");
   Object.keys(props as object)
@@ -30,6 +45,13 @@ const render = (element: AinuElement, parentDom: HTMLElement) => {
     .forEach((name) => {
       const eventType = name.toLowerCase().substring(2);
       dom.addEventListener(eventType, props![name]);
+    });
+
+  const isAttribute = (name: string) => !isListener && name !== "children";
+  Object.keys(props as object)
+    .filter(isAttribute)
+    .forEach((propName) => {
+      dom[propName] = props![propName];
     });
 
   const childElements = props?.children || [];
